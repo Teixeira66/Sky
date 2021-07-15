38b24f0e203fd4d91f8841ae2f8de38aad3158fa
Carlos Teixeira <carlosteixeira@MacBook-Air-de-Carlos.local>
2021-07-16 00:54:05 +0200

    so sorry didnt had more time

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Fetch Data With Axios</h1>
+    <list />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import list from "./components/list.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
-  },
+    list
+  }
 };
 </script>
 
---
+++ src/components/list.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <table border=1px>
+    <tr>
+        <td>Name</td>
+        <td>UserName</td>
+    </tr> 
+
+    </table>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios.axios);
+export default {
+  name: "list",
+  mounted()
+  {
+      Vue.axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((resp)=>{
+          console.warn(resp)
+      })
+  }
+
+}
+</script>
